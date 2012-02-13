d1fa9075c022e759c6520ebc4d636cba9f8cc46d
mayth <chimeaquas@hotmail.com>
2012-02-13 16:01:39 +0900

    Solve 5th

--- mayth/5_120206/ex5_2.c
+++ mayth/5_120206/ex5_2.c
@@ -13,6 +13,7 @@ struct record {
 
 int read_word(FILE *fp, char *word);
 void add_word(char *word);
+int compare_by_freq(const void *s1, const void *s2);
 
 struct record *head = NULL;
 
@@ -21,6 +22,9 @@ int main(int argc, char *argv[])
   FILE *fp;
   char word[WORD_LENGTH];
   struct record *p;
+  int list_size = 0;
+  struct record *sorted = NULL;
+  int i = 0;
 
   if (argc != 2) {
     printf("missing file argument\n");
@@ -40,7 +44,22 @@ int main(int argc, char *argv[])
   fclose(fp);
 
   for (p = head; p != NULL; p = p->next)
-    printf("%s\t%d\n", p->word, p->count);
+    list_size++;
+
+  sorted = (struct record*)malloc(sizeof(struct record) * list_size);
+  if (sorted == NULL)
+  {
+    printf("couldn't allocate memory for sorted list.\n");
+    return 1;
+  }
+
+  for (p = head; p != NULL; p = p->next)
+    sorted[i++] = *p;
+
+  qsort(sorted, list_size, sizeof(struct record), compare_by_freq);
+
+  for (i = 0; i < list_size; i++)
+    printf("%s\t%d\n", sorted[i].word, sorted[i].count);
 
   return 0;
 }
@@ -69,6 +88,9 @@ int read_word(FILE *fp, char *word)
 void add_word(char *word)
 {
   struct record *p = NULL,  *q = NULL, *new = NULL;
+  
+  if (word[0] == '\0') return;
+
   for (p = head; p != NULL; p = p->next)
   {
     if (strcmp(word, p->word) == 0)
@@ -98,15 +120,14 @@ void add_word(char *word)
     new->next = p;
 
     if (q != NULL)
-    {
       q->next = new;
-    }
     else
-    {
-      if (head != NULL)
-        new->next = head;
       head = new;
-    }
   }
 }  
+
+int compare_by_freq(const void *s1, const void *s2)
+{
+  return ((struct record*)s2)->count - ((struct record*)s1)->count;
+}
 
